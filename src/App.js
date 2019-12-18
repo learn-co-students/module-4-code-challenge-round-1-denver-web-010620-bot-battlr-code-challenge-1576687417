8c8fae8a5ebeb1018c270fa336eeda626e03ab7e
github username <github email address>
2019-12-18 11:53:39 -0700

    Mod4 Code Challenge first attempt

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import BotsPage from "./containers/BotsPage";
+import BotCollection from "./containers/BotCollection";
+import BotCard from "./components/BotCard";
 import "./App.css";
 
 class App extends Component {
@@ -7,6 +9,8 @@ class App extends Component {
     return (
       <div className="App">
         <BotsPage />
+        <BotCollection />
+        <BotCard />
       </div>
     );
   }
